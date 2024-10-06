5ace6d22d3bcae6f86e1518c9b2399733b657254
Laura Cheng <laurajiao7@gmail.com>
2024-10-06 19:56:27 +1100

    changed responsive style for weather-detail box

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -3,19 +3,19 @@ import humidityIcon from '../assets/meta_icon/humidity.svg';
 import PMIcon from '../assets/meta_icon/PM2.5.svg';
 import windIcon from '../assets/meta_icon/wind_speed.svg';
 import temperatureIcon from '../assets/meta_icon/Somatosensory_temperature.svg';
-import { getWeatherCategory, weatherBackgroundMap, weatherIconsMap,weatherGradient } from "./ImgResources";
+import { getWeatherCategory, weatherBackgroundMap, weatherIconsMap, weatherGradient } from "./ImgResources";
 import dayjs from 'dayjs'
 
 export default function WeatherDetails({ weatherData }) {
-  if (!weatherData) return <p>Loading weather data...</p>; 
+  if (!weatherData) return <p>Loading weather data...</p>;
 
   const condition = weatherData.current.condition.text.trim();
   const category = getWeatherCategory(condition);
   const currentIcon = weatherIconsMap[category];
   const currentBackground = weatherBackgroundMap[category];
-  const currentWeatherGradient=weatherGradient[category];
-  const currentTime=weatherData.location.localtime;
-  const formattedData=dayjs(currentTime).format('DD-MM-YYYY dddd HH:mm');
+  const currentWeatherGradient = weatherGradient[category];
+  const currentTime = weatherData.location.localtime;
+  const formattedData = dayjs(currentTime).format('DD-MM-YYYY dddd HH:mm');
 
   return (
     <div>
@@ -40,25 +40,33 @@ export default function WeatherDetails({ weatherData }) {
           <img alt={condition} src={currentIcon} />
         </div>
 
-        <div className="weather-info d-flex">
-          <div className="weather-detail-icons">
-            <img alt="humidity" src={humidityIcon} className="icon" />
-            <span>{weatherData.current.humidity}%</span>
+        <div className="weather-info d-flex row">
+          <div className="weather-detail-icons col-lg-3 text-center">
+            <div>
+              <img alt="humidity" src={humidityIcon} className="icon" />
+            </div>
+            <p>{weatherData.current.humidity}%</p>
           </div>
 
-          <div className="weather-detail-icons">
-            <img alt="wind speed" src={windIcon} className="icon wind" />
-            <span>{weatherData.current.wind_kph} km/h</span>
+          <div className="weather-detail-icons col-lg-3 text-center">
+            <div>
+              <img alt="wind speed" src={windIcon} className="icon" />
+            </div>
+            <p>{weatherData.current.wind_kph} km/h</p>
           </div>
 
-          <div className="weather-detail-icons">
-            <img alt="pm2.5" src={PMIcon} className="icon pm" />
-            <span>{weatherData.current.pressure_in} ug</span>
+          <div className="weather-detail-icons col-lg-3 text-center">
+            <div>
+              <img alt="pm2.5" src={PMIcon} className="icon" />
+            </div>
+            <p>{weatherData.current.pressure_in} ug</p>
           </div>
 
-          <div className="weather-detail-icons">
-            <img alt="temperature" src={temperatureIcon} className="icon" />
-            <span>{weatherData.current.temp_f}&deg;F</span>
+          <div className="weather-detail-icons col-lg-3 text-center">
+            <div>
+              <img alt="temperature" src={temperatureIcon} className="icon" />
+            </div>
+            <p>{weatherData.current.temp_f}&deg;F</p>
           </div>
         </div>
       </div>
